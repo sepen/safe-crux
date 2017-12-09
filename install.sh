a77da8fe51e6ef88d776dc2383aaa5663dcd891f
Jose V Beneyto <sepen@crux.nu>
2017-12-09 19:31:41 +0000

    Use env variables during installation

--- install.sh
+++ install.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-export DESTDIR=
-
-if [ $# -ne 0 ]; then
-  [ ! -d $1 ] mkdir -p $1
-  DESTDIR=$1
-fi
-
-export PREFIX=/opt
-export BINDIR=${PREFIX}/bin
-export DATADIR=${PREFIX}/scx
+[ -z $DESTDIR ] && export DESTDIR=
+[ -z $PREFIX ]  && export PREFIX=/usr/local
+[ -z $BINDIR ]  && export BINDIR=${PREFIX}/bin
+[ -z $DATADIR ] && export DATADIR=${PREFIX}/scx
 
 install -v -d ${DESTDIR}${BINDIR}
 sed "s|BASEDIR=.*|BASEDIR=${DATADIR}|" safe-crux.wrapper.in > ${DESTDIR}${BINDIR}/safe-crux
